2d351008a22d7412e2323200b2a2cdfe3e2b78df
Pablo Ramos <pablo-362@hotmail.com>
2022-09-13 20:42:47 -0300

    add input

--- pages/flow-chart.tsx
+++ pages/flow-chart.tsx
@@ -1,12 +1,35 @@
 import type {NextPage} from "next";
 
 import Head from "next/head";
+import React, {useRef, useState} from "react";
 
 import styles from "../styles/Home.module.css";
-import {Navbar} from "../components/Navbar";
 import {Flowchart} from "../components/Flowchart";
 
 const FlowChart: NextPage = () => {
+  const [isEditable, setIsEditable] = useState(false);
+  const ref = useRef(null);
+  const refButton = useRef(null);
+
+  const handleEdit = (event: React.MouseEvent<any>) => {
+    console.log("aluco", event.currentTarget);
+    // if (event.currentTarget.id === "buttoncito") {
+    //   console.log("if");
+    //   setIsEditable(false);
+    // } else {
+    //   console.log("else");
+
+    //   setIsEditable(true);
+    //   //@ts-ignore
+    //   ref.current.focus();
+    // }
+  };
+
+  const handleOnBlur = () => {
+    //@ts-ignore
+    setIsEditable(false);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -25,6 +48,19 @@ const FlowChart: NextPage = () => {
           width: "100vw",
         }}
       >
+        <div>
+          <h3>isEditable {JSON.stringify(isEditable)}</h3>
+          <input
+            ref={ref}
+            placeholder="apretame"
+            readOnly={!isEditable}
+            onBlur={handleOnBlur}
+            onClick={handleEdit}
+          />
+          <button ref={refButton} id="buttoncito" onClick={() => setIsEditable(!isEditable)}>
+            hola
+          </button>
+        </div>
         <Flowchart />
       </main>
     </div>
